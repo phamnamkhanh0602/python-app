7c7fb5eb136a62157a102cf4df3738be5d9fbee6
phamnamkhanh0602 <105868231+phamnamkhanh0602@users.noreply.github.com>
2025-03-14 20:18:51 +0700

    tufyf

--- weather_api.py
+++ weather_api.py
@@ -2,7 +2,7 @@ import requests
 api_key = "5634849698f284eba828945eec5edfee"
 
 
-city = "Ha Noi"
+city = "Aomori"
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
 print(url)
 x = requests.get(url)
